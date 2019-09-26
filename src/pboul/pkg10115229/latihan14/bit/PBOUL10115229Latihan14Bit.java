537ff7e49e1910e8e2b4e6cb4250b67632143a6b
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 10:23:53 +0700

    project keempatbelas

---
+++ src/pboul/pkg10115229/latihan14/bit/PBOUL10115229Latihan14Bit.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan14.bit;
+
+/**
+ *
+ * @author Lukman
+ */
+public class PBOUL10115229Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 60;/* 60 = 011 110 */
+        int b = 13;/* 13 = 0000 1101 */
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /*-61 = 0011 1101 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /*-61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+    
+}
